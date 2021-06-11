c6d11d378df779f8cb1bf3e349ecf697c15c08d6
Станислав <yastas593@mail.ru>
2021-06-11 09:24:21 +0300

    bootstr

--- src/main/java/com/spring_boot/springbootproj/SpringSecurity/handler/LoginSuccessHandler.java
+++ src/main/java/com/spring_boot/springbootproj/SpringSecurity/handler/LoginSuccessHandler.java
@@ -20,11 +20,11 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException, ServletException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ROLE_ADMIN")) {
-            httpServletResponse.sendRedirect("/UsersInfo");
+            httpServletResponse.sendRedirect("/admin");
         } else if(roles.contains("ROLE_USER")) {
-            httpServletResponse.sendRedirect("/");
+            httpServletResponse.sendRedirect("/user");
         } else {
-            httpServletResponse.sendRedirect("/");
+            httpServletResponse.sendRedirect("/user");
         };
     }
 }
--- src/main/java/com/spring_boot/springbootproj/controllers/UsersInfo.java
+++ src/main/java/com/spring_boot/springbootproj/controllers/UsersInfo.java
@@ -24,19 +24,19 @@ public class UsersInfo {
         return "index";
     }
 
-    @GetMapping(value = "/UsersInfo")
-    public String postInfo(Model model){
+    @GetMapping(value = "/admin")
+    public String postInfo(Model model, Principal principal){
         model.addAttribute("roles", us.getAllRoles());
         model.addAttribute("users", us.getAllUsers());
         model.addAttribute("user", new User());
         model.addAttribute("role", new Role());
-        System.out.println(us.findUserByLogin("2").toString());
-        return "usersInfo";
+        model.addAttribute("autUser", us.findUserByLogin(principal.getName()));
+        return "admin";
     }
 
-    @GetMapping(value = "/User")
+    @GetMapping(value = "/user")
     public String UserStr(Model model, Principal principal){
-        model.addAttribute("User", us.findUserByLogin(principal.getName()));
+        model.addAttribute("user", us.findUserByLogin(principal.getName()));
         return "user";
     }
 
@@ -44,14 +44,14 @@ public class UsersInfo {
     @GetMapping(value = "/deleteUser")
     public String deleteUser(@RequestParam("id") long id){
         us.deleteUser(id);
-        return "redirect:/UsersInfo";
+        return "redirect:/admin";
     }
 
     @GetMapping(value = "/addUser")
     public String addUser(@ModelAttribute("user") User user, @RequestParam(name = "roleName") String roleName){
         user.addRoleToUser(us.getRoleByName(roleName));
         us.addUser(user);
-        return "redirect:/UsersInfo";
+        return "redirect:/admin";
     }
 
     @GetMapping(value = "/updateUser")
@@ -72,7 +72,7 @@ public class UsersInfo {
             user.addRoleToUser(us.getRoleByName(roleName));
             us.updateUser(user);
         }
-        return "redirect:/UsersInfo";
+        return "redirect:/admin";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
